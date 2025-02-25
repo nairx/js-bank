2cd32a81eba6abb5f1f7dd5700e3da9bc927861b
nairx <nairxprv@gmail.com>
2025-02-25 09:32:21 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 let users = [];
 let user = {};
-// let useremail = "";
-// let username = "";
-// let currBalance = 0;
 document.write("<div id=root></div>");
 function toggle(color) {
   if (color == "dark") {
